9a6d5edb6742b8a6fd4deba3d2373b2391726ece
Chiragd42 <87471474+Chiragd42@users.noreply.github.com>
2023-04-22 23:06:02 +0530

    Add files via upload

---
+++ TaskManager.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/tasks', (req, res) => {
+    console.log("Welcome to the task manager ");
+    res.json(tasks);
+});
+
+
+
+app.get('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const task = tasks.find(task => task.id === id);
+    if (!task) {
+        return res.status(404).send('Task not found');
+    }
+    res.json(task);
+});
+
+
+app.post('/tasks', validateTask, (req, res) => {
+    const { title, description, completed } = req.body;
+    const newTask = { id: uuidv4(), title, description, completed };
+    tasks.push(newTask);
+    res.json(newTask);
+});
+
+
+wa
+
+app.put('/tasks/:id', validateTask, (req, res) => {
+    const { id } = req.params;
+    const { title, description, completed } = req.body;
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    if (taskIndex === -1) {
+        return res.status(404).send('Task not found');
+    }
+    const updatedTask = { ...tasks[taskIndex], title, description, completed };
+    tasks[taskIndex] = updatedTask;
+    res.json(updatedTask);
+});
+
+
+
+
+app.delete('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    if (taskIndex === -1) {
+        return res.status(404).send('Task not found');
+    }
+    tasks.splice(taskIndex, 1);
+    res.sendStatus(204);
+})
+
